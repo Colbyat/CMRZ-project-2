1797cc38822659fed4aac022aadb69320aea779b
Colby Thompson <colbyat@email.arizona.edu>
2020-10-27 16:01:58 -0700

    cleaned up workspace

---
+++ ePatient/src/com/util/HibernateSessionFactory.java
@@ -0,0 +1,24 @@
+package com.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactory {
+	
+	private static SessionFactory sessionFactory;
+	
+	public static Session getSession() {
+		
+		if(sessionFactory == null) {
+			sessionFactory = new Configuration().configure()
+					.setProperty("hibernate.connection.url", System.getenv("dburl")) 
+					.setProperty("hibernate.connection.username", System.getenv("dbusername"))
+					.setProperty("hibernate.connection.password", System.getenv("dbpassword"))
+					.buildSessionFactory();
+		}
+		
+		return sessionFactory.getCurrentSession();
+	}
+}
+
