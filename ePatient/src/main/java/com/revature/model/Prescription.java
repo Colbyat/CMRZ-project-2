1536fb85471b55af1df08c559eba0800d010b475
zach scharoun <zscharoun@gmail.com>
2020-10-27 18:03:22 -0400

    Adds object models

---
+++ ePatient/src/main/java/com/revature/model/Prescription.java
@@ -0,0 +1,11 @@
+package com.revature.model;
+
+public class Prescription
+{
+    private int id;
+    private Medication medication;
+    private Person prescribedBy;
+    private Person prescibedTo;
+    private int dosage;
+    
+}
