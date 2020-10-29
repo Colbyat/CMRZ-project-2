3542f0966735536e5b947e16959ae1616098297f
Rabinson Shrestha <rabinshrestha4100@gmail.com>
2020-10-28 20:02:37 -0500

    - Deleted GetAuth.java and moved the logic to   HibernateSessionFactory.java
    - Test works

--- ePatient/src/main/java/com/revature/utility/HibernateSessionFactory.java
+++ ePatient/src/main/java/com/revature/utility/HibernateSessionFactory.java
@@ -1,28 +1,76 @@
 package com.revature.utility;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibernateSessionFactory
-{
-	private static final String USERNAME = GetAuth.property.getProperty("user");
-	private static final String PASSWORD = GetAuth.property.getProperty("password");
-	private static final String URL = GetAuth.property.getProperty("url");
+public class HibernateSessionFactory {
 
-    private static SessionFactory sessionFactory;
-	
-    public static Session getSession() 
-    {	
-	if(sessionFactory == null) 
-	{
-		sessionFactory = new Configuration().configure()
-			.setProperty("hibernate.connection.url", URL) 
-			.setProperty("hibernate.connection.username", USERNAME)
-			.setProperty("hibernate.connection.password", PASSWORD)
-			.buildSessionFactory();
+	private static SessionFactory sessionFactory;
+
+	public static Session getSession() {
+		if (sessionFactory == null) {
+
+			// property obj to hold our connection info
+			Properties auth = new Properties();
+
+			// read properties from file
+			FileReader fr = null;
+			String filePath = "R:\\Revature\\9-21-2020-jwa\\Project Submissions\\project 1\\.ignore\\.auth\\connection.prop";
+
+			try {
+				fr = new FileReader(filePath);
+			} catch (FileNotFoundException e) {
+				System.out.println("File " + filePath + " not found!");
+				System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
+			}
+
+			BufferedReader br = new BufferedReader(fr);
+
+			String str = "";
+
+			try {
+				while ((str = br.readLine()) != null) {
+					String[] parts = str.split(".=.");
+					if (parts[0].equals("url"))
+						auth.put("url", parts[1]);
+					else if (parts[0].equals("uname")) {
+						auth.put("user", parts[1]);
+					} else if (parts[0].equals("pass")) {
+						auth.put("password", parts[1]);
+					}
+				}
+			} catch (IOException e) {
+				System.out.println(
+						"Error in your connection.prop file! Check if key and value are delimeted by .=. ! Also check if the key names are correct and their corresponding values are correct too!");
+				e.printStackTrace();
+				System.out.println("=======================================================================");
+			} finally {
+				try {
+					br.close();
+					fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			/*
+			System.out.println(auth.getProperty("url"));
+			System.out.println(auth.getProperty("user"));
+			System.out.println(auth.getProperty("password"));
+			*/
+
+			sessionFactory = new Configuration().configure()
+					.setProperty("hibernate.connection.url", auth.getProperty("url"))
+					.setProperty("hibernate.connection.username", auth.getProperty("user"))
+					.setProperty("hibernate.connection.password", auth.getProperty("password"))
+					.buildSessionFactory();
+		}
+		return sessionFactory.getCurrentSession();
 	}
-		
-	return sessionFactory.getCurrentSession();
-    }
 }
